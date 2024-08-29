3f6fc4d232a2799dec25c969df5ec2231b987e9d
sardorbek2024 <168269584+sardorbek2024@users.noreply.github.com>
2024-08-29 11:11:28 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let a = +prompt("birinchi raqam kirting")
+let b = prompt("qiymatni kiritng")
+let c = +prompt("ikkinchi soni kiritng !")
+
+if(c == "%"){
+    alert(a * c / 100)
+}
+
+else{
+    alert("notg'ri soni kiritng")
+}
